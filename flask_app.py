da9ef31b742513ba491925a7cbdcc4dd83690145
Mark Hartnady <mark.hartnady@vrpconsulting.com>
2023-03-08 14:21:44 +0000

    Update March 8th, 2023

--- flask_app.py
+++ flask_app.py
@@ -11,15 +11,21 @@ app.config.from_object('config')
 app.config['DEBUG'] = True
 openai.api_key = app.config['OPEN_AI_KEY']
 slack_webhook_url = app.config['SLACK_WEBHOOK_URI']
+slack_api_key = app.config['SLACK_API_KEY']
+chat_bot_user_id = app.config['CHAT_BOT_USER_ID']
 
 db = SQLAlchemy(app)
+
+#====================
+#PDF EXTRACT METHODS:
+#====================
 
 def gpt_complete(prompt):
     params = {
         "prompt":prompt,
         "engine":"text-davinci-003",
-        "temperature":0.1,
-        "max_tokens":4097-len(prompt)-150
+        "temperature":0.1, #please keep temperature low for PDF entity extraction
+        "max_tokens":4097-len(prompt)-150 #please do use an imported module to calculate accurate token size (it slows down processing)
     }
     response = openai.Completion.create(**params)
 
@@ -35,6 +41,26 @@ def gpt_complete(prompt):
     except Exception as e:
         error_traceback = traceback.format_exc()
         return f"REST Error: {str(e)}\n{error_traceback}"
+
+#======================
+#GPT SLACK BOT METHODS:
+#======================
+
+def get_last_5_requests(user_id):
+    prev_requests = Job.query.filter_by(user_id=user_id).order_by(Job.id.desc()).limit(5)
+    prev_req_ids = list()
+    for prev_job in prev_requests: prev_req_ids.append(str(prev_job.id))
+    prev_req_ids_str = ', '.join(prev_req_ids)
+    return prev_req_ids_str
+
+def responder(job):
+    msg = job.message.replace('\n','\n>')
+    resp = job.response.replace('\n','\n>')
+    return f"*Request:* `{job.id}`\n*IP & Timestamp*: `{job.slug}`\n*<@{job.user_id}> asked:*\n>{msg}\n*GPT Responded:*\n>{resp}"
+
+#======================
+#FLASK ROUTINGS (APIs):
+#======================
 
 @app.route('/', methods=['GET'])
 def root_get():
@@ -70,27 +96,28 @@ def slack_post_to_channel_post():
 
     mimetype = request.mimetype
     if mimetype == 'application/x-www-form-urlencoded':
-        payload = request.form
+        data = request.form
+        if 'payload' not in data: abort(500,description='"payload" value missing from form data')
+        payload = json.loads(data['payload'])
     else:
         abort(500,description="This API only accepts mime-type of application/x-www-form-urlencoded")
 
-    if 'payload' not in payload: abort(500,description='"payload" value missing from form data')
-    #if 'user' not in payload['payload']: abort(500,description='"user" element missing from payload')
-    if 'message' not in payload['payload']: abort(500,description='"message" element missing from payload')
+    job_id = payload['actions'][0]['value']
+    job = Job.query.filter_by(id=job_id).first()
 
-    payload = payload['payload']
-    payload = json.loads(payload)
-    #user_id = payload['user']['id']
-    message_text = payload['message']['text']
+    outbound_payload = { "channel": f"{job.channel_id}", "text": responder(job) }
 
-    outbound_payload = { "text": f"{message_text}" }
+    headers = headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Bearer " + slack_api_key
+    }
 
-    response = requests.post(slack_webhook_url, json=outbound_payload)
+    response = requests.post('https://slack.com/api/chat.postMessage', json=outbound_payload, headers=headers)
 
     if response.status_code == 200:
         return '', 200
     else:
-        abort(500,description='Error posting to public channel via webhook.')
+        return 'Error posting message. Please check server logs.', 200
 
 @app.route('/slack/events', methods=['GET'])
 def slack_events_get():
@@ -109,50 +136,114 @@ def slack_events_post():
     if 'user_id' not in payload: abort(500,description='"user_id" value missing from form data')
 
     text = payload['text']
+    user_id = payload['user_id']
+    if 'channel_id' in payload: channel_id = payload['channel_id']
+
+    if text.strip() == '':
+        return 'No prompt detected. Try again using syntax: `/gpt your question here` or `/gpt help` for help.', 200
 
     if text == 'help':
-        return_text =  '\n>`/gpt [your_prompt]` will queue a *new* request with the GPT service. e.g. `/gpt give me a chocolate cake recipe`'
-        return_text += '\n>`/gpt [request_id]` will provide the status of a *queued* request e.g. `/gpt 25`'
-        return_text += '\n>`/gpt [request_id] [your_next_prompt]` will continue a *previous* dialogue e.g. `/gpt 25 now give me baking instructions`'
+        return_text =  ' \n>`/gpt [your_prompt]` will enqueue a *new* request with the GPT service. e.g. `/gpt give me a chocolate cake recipe`'
+        return_text += '\n>`/gpt [request_id]` will provide the status of a *previous request* e.g. `/gpt 255`'
+        return_text += '\n>`/gpt [request_id] [your_next_prompt]` will continue a *previous dialogue* e.g. `/gpt 255 now give me baking instructions`'
+        return_text += '\n>`/gpt share [request_id] with [slack_user]` will share your request with another user e.g. `/gpt share 255 with firstname.lastname`'
+        return_text += '\n>`/gpt list` will list your last 5 request ids'
+        return_text += '\n>All GPT responses will be sent in a private message back to the requesting user.'
+        return_text += '\n>To post the response publically to the channel where you issued the request, you can click `Post to Channel`.'
+        return_text += '\n>For this to work, you must first `/invite @GPT` to the channel'
         count = Job.query.filter_by(state='queued').count()
         return_text += '\nMax prompt length is *3000 characters*'
         return_text += f'\nThere are currently *{count} items* in the queue awaiting processing.'
         return return_text, 200
 
+    if text == 'list':
+        return f'Your last 5 request ids: `{get_last_5_requests(user_id)}`', 200
+
     if len(text) > 3000:
         return 'Prompt too long. Max characters in prompt = 3000', 200
 
-    if len(text) < 30 and text.isdigit():
+    if len(text) < 20 and text.isdigit():
         int_id = int(text)
         queue = Job.query.filter_by(id=int_id)
         if job := queue.first():
-            count = Job.query.filter_by(state='queued').count()
-            return 'Request:'+str(job.id) + '\nUser: <@'+str(job.user_id)+'>\nText: '+str(job.message)+'\nState: ' + str(job.state) + '\nQueue size: ' + str(count), 200
-        else:
-            return 'Request:'+str(text) + ' not found.', 200
 
-    user_id = payload['user_id']
+            if job.user_id != user_id and user_id != 'U02B74RS2MT': #user_id != 'U02B74RS2MT' is for debugging purposes -- will be removed
+                return f'Naughty naughty! Request `{job.id}` was not your request. You can only query your own requests.\nYour last 5 requests were: `{get_last_5_requests(user_id)}`', 200
+
+            #count = Job.query.filter_by(state='queued').count()
+            return responder(job), 200
+        else:
+            return f'Request: `{text}` not found.', 200
 
     text_parts = text.split(' ')
     if len(text_parts) > 1:
         if text_parts[0].isdigit():
             prev = Job.query.filter_by(user_id=user_id).order_by(Job.id.desc())
+
             if job := prev.first():
-                everything_after_the_request_id = ' '.join(text_parts[1:])
-                text = job.message + '\n' + job.response + '\n' + everything_after_the_request_id
-                text = text[-3000:] if len(text) > 3000 else text
+                if job.user_id != user_id:
+                    return f'Naughty naughty! Request `{prev.id}` was not your request. You can only continue your own dialogue.', 200
+                else:
+                    everything_after_the_request_id = ' '.join(text_parts[1:])
+                    text = job.message + '\n' + job.response + '\n' + everything_after_the_request_id
+                    text = text[-3000:] if len(text) > 3000 else text
+
+        elif text_parts[0] == 'share':
+            if text_parts[1].isdigit() and len(text_parts) > 3 and text_parts[2] == 'with':
+                prev = Job.query.filter_by(user_id=user_id, id=int(text_parts[1]))
+                if job := prev.first():
+
+                    email_lookup = text_parts[3]
+
+                    if '@vrpconsulting.com' not in email_lookup: email_lookup += '@vrpconsulting.com'
+
+                    headers = {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": f"Bearer {slack_api_key}"
+                    }
+
+                    response = requests.get('https://slack.com/api/users.lookupByEmail',f'email={email_lookup}', headers=headers)
+
+                    if response.status_code == 200 and 'user' in response.json():
+                        response_json = response.json()
+                        email_lookup_user_id = response_json['user']['id']
+                        headers = {
+                            "Content-Type": "application/json; charset=utf-8",
+                            "Authorization": f"Bearer {slack_api_key}"
+                        }
+                        payload = {
+                            "channel": f"{email_lookup_user_id}",
+                            "text": responder(job)
+                        }
+
+                        user_response = requests.post('https://slack.com/api/chat.postMessage',json=payload, headers=headers)
+                        if user_response.status_code == 200:
+                            return f'Request `{job.id}` sent as private message to <@{email_lookup_user_id}> by the GPT Slack App.', 200
+                        else:
+                            return f'Request `{job.id}` could not be sent to <@{email_lookup_user_id}>.\nResponse code: `{user_response.status_code}`', 200
+                    else:
+                        return f'User `{text_parts[3]}` not found. Please use the following syntax: `/gpt share request_id with firstname.lastname`', 200
+
+                else:
+                    return f'Request `{text_parts[1]}` not found (or it\'s not yours to share). To list your last 5 requests use: `/gpt list`'
+
+            else:
+                return 'Are you trying to share a request? Use syntax: `/gpt share [request_id] with [firstname.lastname]` e.g. `/gpt share 255 with joe.bloggs`'
 
     ip = request.headers.get("X-Real-Ip", "")
     now = datetime.utcnow().isoformat()
-    job_id = f"{ip}{now}"
+    job_id = f"{ip} {now}"
 
-    data = Job(slug=job_id,message=text,webhook_url=slack_webhook_url,user_id=user_id)
+    if channel_id:
+        data = Job(slug=job_id,message=text,channel_id=channel_id,user_id=user_id)
+    else:
+        data = Job(slug=job_id,message=text,webhook_url=slack_webhook_url,user_id=user_id)
     db.session.add(data)
     db.session.commit()
 
-    count = Job.query.filter_by(state='queued').count()
+    #count = Job.query.filter_by(state='queued').count()
 
-    return f'Request placed in queue with `id: {data.id}`.\nThere are {count} items in the queue including yours.\nTo query the status of this request, type `/gpt {data.id}`', 200
+    return f'Request placed in queue with id: `{data.id}`.\nTo query the status of this request, type `/gpt {data.id}`', 200
 
 @app.route("/slack/events/<string:job_id>", methods=["GET"])
 def slack_events_status_get(job_id):
@@ -170,6 +261,8 @@ def slack_events_status_get(job_id):
         }
     )
 
+#INNER CLASSES
+
 class Job(db.Model):
     __tablename__ = "jobs"
     id = db.Column(db.Integer, primary_key=True)
@@ -177,6 +270,7 @@ class Job(db.Model):
     webhook_url = db.Column(db.String(500), nullable=True)
     message = db.Column(db.String(65535), nullable=True)
     user_id = db.Column(db.String(20), nullable=True)
+    channel_id = db.Column(db.String(20), nullable=True)
     state = db.Column(db.String(10), nullable=False, default="queued")
     result = db.Column(db.Integer, default=0)
     response = db.Column(db.String(65535), nullable=True)
--- processing.py
+++ processing.py
@@ -1,10 +1,17 @@
+import smtplib
+from email.message import Message
+from email.utils import formataddr
+from smtplib import SMTPAuthenticationError
+
 import requests, openai, traceback
 from time import sleep
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_ENGINE_OPTIONS, OPEN_AI_KEY, SLACK_API_KEY
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_ENGINE_OPTIONS
+from config import OPEN_AI_KEY, SLACK_API_KEY, CHAT_BOT_USER_ID
+from config import ERROR_SERVICE_SENDER, ERROR_SERVICE_SENDER_TOKEN, ERROR_SERVICE_RECIPIENT
 from flask_app import Job
 
 openai.api_key = OPEN_AI_KEY
@@ -14,7 +21,47 @@ engine = create_engine(
 )
 Session = sessionmaker(engine)
 
-def gpt_max(prompt):
+def send_email(sender_email, sender_password, receiver_email, subject, body, sender_name='PythonAnywhere Exception'):
+    try:
+        # Create the message object
+        message = Message()
+        message['From'] = formataddr((sender_name, sender_email)) if sender_name else sender_email
+        message['To'] = receiver_email
+        message['Subject'] = subject
+        message.set_payload(body)
+
+        # Connect to the SMTP server
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()  # Start TLS encryption
+            server.login(sender_email, sender_password)  # Login to the SMTP server
+            text = message.as_string()
+            server.sendmail(sender_email, receiver_email, text)  # Send the email
+    except SMTPAuthenticationError as e:
+        print(f"Failed to send email due to authentication error: {e}")
+    except Exception as e:
+        print(f"Failed to send email due to an error: {e}")
+
+def gpt_chat(prompt):
+    params = {
+        "model": "gpt-3.5-turbo",
+        "max_tokens":4097-len(prompt)-100,
+        "temperature":0.5,
+        "messages": [{"role": "user", "content": f"{prompt}"}]
+    }
+    try:
+        response = openai.ChatCompletion.create(**params)
+
+        if 'error' in response:
+            error_message = response['error']['message']
+            return f"GPT Error: {error_message}"
+        else:
+            completion_text = response.choices[0]['message']['content'].strip()
+            return completion_text
+    except Exception as e:
+        error_traceback = traceback.format_exc()
+        return f"REST Error: {str(e)}\n{error_traceback}"
+
+'''def gpt_complete(prompt):
     params = {
         "prompt":prompt,
         "engine":"text-davinci-003",
@@ -32,58 +79,102 @@ def gpt_max(prompt):
             return completion_text
     except Exception as e:
         error_traceback = traceback.format_exc()
-        return f"REST Error: {str(e)}\n{error_traceback}"
+        return f"REST Error: {str(e)}\n{error_traceback}"'''
 
 def find_pending_job():
     with Session.begin() as session:
         queue = session.query(Job).filter_by(state="queued")
         if job := queue.first():
             job.state = "processing"
+            job.response = "Processing..."
             return job.id
+
+def bot_is_member_of_channel(channel_id, bot_id):
+    form_headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Bearer " + SLACK_API_KEY
+    }
+    response = requests.get('https://slack.com/api/conversations.members',f'channel={channel_id}',headers=form_headers)
+    if response.status_code == 200:
+        resp_json = response.json()
+        if 'members' in resp_json:
+            for member_id in resp_json['members']:
+                if member_id == bot_id: return True
+
+    return False
+
+def responder(job,gpt_resp):
+    msg = job.message.replace('\n','\n>')
+    resp = gpt_resp.replace('\n','\n>')
+    return f"*Request:* `{job.id}`\n*<@{job.user_id}> asked:*\n>{msg}\n*GPT Responded:*\n>{resp}"
+    #return f"*Request:* `{job.id}`:\n<@{job.user_id}> asked: {job.message}\n*GPT Responded:*\n>{gpt_resp}"
+
+def PrivateDirectMessage(job, gpt_resp):
+    global SLACK_API_KEY
+    payload = {
+            "channel": f"{job.user_id}",
+            "text": responder(job,gpt_resp)
+    }
+    headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer " + SLACK_API_KEY
+    }
+
+    return requests.post('https://slack.com/api/chat.postMessage', json=payload, headers=headers)
+
+def PrivateMessageInChannel(job, gpt_resp):
+    global SLACK_API_KEY
+    payload = {
+            "channel": f"{job.channel_id}",
+            "user": f"{job.user_id}",
+            "blocks": [
+        		{
+        			"type": "section",
+        			"text": {
+        				"type": "mrkdwn",
+        				"text": responder(job,gpt_resp)
+        			},
+        			"accessory": {
+        				"type": "button",
+        				"text": {
+        					"type": "plain_text",
+        					"text": "Post to Channel"
+        				},
+        				"value": f"{job.id}",
+        				"action_id": "button-action"
+        			}
+        		}
+        	]
+    }
+
+    headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer " + SLACK_API_KEY
+    }
+
+    return requests.post('https://slack.com/api/chat.postEphemeral', json=payload, headers=headers)
 
 def process_job(job_id):
     print(f"Processing job: {job_id}...", end=" ", flush=True)
 
     with Session.begin() as session:
         job = session.query(Job).filter_by(id=job_id).first()
-        gpt_resp = gpt_max(job.message)
+        gpt_response = gpt_chat(job.message) #gpt_complete(job.message)
 
-        gpt_resp = gpt_resp.replace('\n','\n>')
-
-        payload = {
-            "channel": f"{job.user_id}",
-            "blocks": [
-        		{
-        			"type": "section",
-        			"text": {
-        				"type": "mrkdwn",
-        				"text": f"<@{job.user_id}> asked: {job.message}\n>{gpt_resp}"
-        			},
-        			"accessory": {
-        				"type": "button",
-        				"text": {
-        					"type": "plain_text",
-        					"text": "Post to GPT Channel"
-        				},
-        				"value": "click_me_123",
-        				"action_id": "button-action"
-        			}
-        		}
-        	]
-        }
-
-        headers = {
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": "Bearer " + SLACK_API_KEY
-        }
-
-        response = requests.post('https://slack.com/api/chat.postMessage', json=payload, headers=headers)
+        if job.channel_id is None or job.channel_id == '':
+            response = PrivateDirectMessage(job,gpt_response)
+        else:
+            c_id = job.channel_id
+            if c_id[0] == 'C' and bot_is_member_of_channel(c_id,CHAT_BOT_USER_ID):
+                response = PrivateMessageInChannel(job,gpt_response)
+            else:
+                response = PrivateDirectMessage(job,gpt_response)
 
         if response.status_code == 200:
 
             with Session.begin() as session:
                 session.query(Job).filter_by(id=job_id).update(
-                    {"result": 1, "state": "completed", "response": gpt_resp}
+                    {"result": 1, "state": "completed", "response": gpt_response}
                 )
 
             print(f"{job_id} is complete.")
@@ -92,17 +183,33 @@ def process_job(job_id):
 
             with Session.begin() as session:
                 session.query(Job).filter_by(id=job_id).update(
-                    {"result": 0, "state": "failed"}
+                    {"result": 0, "state": "failed" }
                 )
 
             print(f"{job_id} failed to process.")
             print(f"{response.content}")
 
 
-
 if __name__ == "__main__":
-    while True:
-        if job_id := find_pending_job():
-            process_job(job_id)
-        else:
-            sleep(1)
+    print('Waiting for first job...')
+    print('Press "Control + C" to halt processing')
+    email_subj = 'GPT Slack for VRP Queue Worker Failure'
+
+    try:
+
+        while True:
+
+            if job_id := find_pending_job():
+                process_job(job_id)
+                print('Waiting for next job...')
+                if int(job_id) % 15 == 0: print('Press "Control + C" to halt processing')
+            else:
+                sleep(1)
+
+    except KeyboardInterrupt:
+
+        send_email(ERROR_SERVICE_SENDER, ERROR_SERVICE_SENDER_TOKEN, ERROR_SERVICE_RECIPIENT, email_subj, 'Script was halted due to keyboard interrupt')
+
+    except Exception as e:
+
+        send_email(ERROR_SERVICE_SENDER, ERROR_SERVICE_SENDER_TOKEN, ERROR_SERVICE_RECIPIENT, email_subj, e)
